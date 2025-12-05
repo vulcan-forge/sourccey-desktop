a6a7558ed5111da0cfc8b6351412e5874ef27ead
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-12-05 17:10:49 -0500

    before and after read

--- src/utils/config/calibration.ts
+++ src/utils/config/calibration.ts
@@ -1,6 +1,8 @@
 import { invoke } from '@tauri-apps/api/core';
 
 export const readCalibration = async (robot_type: string, nickname: string) => {
+    console.log('before read');
     const calibration = await invoke('read_calibration', { robot_type, nickname });
+    console.log('after read calibration', calibration);
     return calibration;
 };
