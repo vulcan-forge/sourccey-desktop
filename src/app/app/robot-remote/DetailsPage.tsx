2c7c9fc0363d12984b3ecc7676490de31b12835c
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-12-05 15:15:50 -0500

    desktop calibration fix

--- src/app/app/robot-remote/DetailsPage.tsx
+++ src/app/app/robot-remote/DetailsPage.tsx
@@ -12,9 +12,16 @@ export const KioskRobotDetailsPage: React.FC = () => {
     const { data: calibration }: any = useGetCalibration(nickname);
     const { data: isCalibrated }: any = useGetIsCalibrated();
 
+    console.log(isCalibrated);
+    console.log(calibration);
+
     return (
         <div className="bg-slate-850 flex h-screen flex-col">
             <div className="w-full space-y-4 p-6">
+                <div className="flex items-center gap-2">
+                    <h1 className="text-2xl font-bold">Robot Calibration:</h1>
+                    <span className="text-sm text-slate-400">{isCalibrated ? 'Calibrated' : 'Not Calibrated'}</span>
+                </div>
                 {isCalibrated && calibration && <RobotControl nickname={nickname} />}
                 <RobotKioskCalibration nickname={nickname} robotType={robotType} calibration={calibration} />
             </div>
