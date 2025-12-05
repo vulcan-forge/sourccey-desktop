d0ff9a6845fc8d5cbfc60a946615ba535fade592
Nicholas Maselli <maselli.nicholas@gmail.com>
2025-12-05 17:17:03 -0500

    testing command prints

--- src-tauri/src/modules/control/controllers/configuration/calibration_controller.rs
+++ src-tauri/src/modules/control/controllers/configuration/calibration_controller.rs
@@ -25,13 +25,15 @@ pub struct RemoteCalibrationConfig {
 }
 
 #[tauri::command]
-pub fn read_calibration(robot_type: String, nickname: String) -> Result<(Calibration, bool), String> {
+pub fn read_calibration(nickname: String) -> Result<(Calibration, bool), String> {
     println!("--------------------------------");
-    println!("robot_type: {:?}", robot_type);
+    // println!("robot_type: {:?}", robot_type);
     println!("nickname: {:?}", nickname);
     println!("--------------------------------");
-    let (calibration, is_calibrated) = CalibrationService::read_calibration(&robot_type, &nickname)?;
-    Ok((calibration, is_calibrated))
+    Ok((Calibration { motors: HashMap::new() }, false))
+    println!("--------------------------------");
+    // let (calibration, is_calibrated) = CalibrationService::read_calibration(&robot_type, &nickname)?;
+    // Ok((calibration, is_calibrated))
 }
 
 #[tauri::command]
