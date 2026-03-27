b1cecfcbe7b7e3dfa440d5588814ad04ffea929c
Nicholas Maselli <maselli.nicholas@gmail.com>
2026-03-27 02:58:01 -0400

    path constants

--- src-tauri/src/services/directory/path_constants.rs
+++ src-tauri/src/services/directory/path_constants.rs
@@ -3,7 +3,6 @@ use std::path::PathBuf;
 
 pub const REMOTE_HOME_DIR: &str = "/home/sourccey";
 pub const SOURCCEY_DESKTOP_ROOT: &str = "/home/sourccey/Desktop/Projects/sourccey-desktop";
-pub const LEROBOT_VULCAN_ROOT: &str = "/home/sourccey/Desktop/Projects/sourccey-desktop/lerobot-vulcan";
 
 /// Helper function to get remote home dir as PathBuf
 #[allow(dead_code)]
@@ -15,11 +14,4 @@ pub fn get_remote_home_dir() -> PathBuf {
 pub fn get_project_root() -> PathBuf {
     PathBuf::from(SOURCCEY_DESKTOP_ROOT)
 }
-
-pub fn get_lerobot_vulcan_root() -> PathBuf {
-    PathBuf::from(LEROBOT_VULCAN_ROOT)
-}
-
-
-
 
--- src-tauri/src/services/directory/remote_directory_service.rs
+++ src-tauri/src/services/directory/remote_directory_service.rs
@@ -15,14 +15,4 @@ impl RemoteDirectoryService {
     pub fn get_sourccey_desktop_root() -> Result<PathBuf, String> {
         Ok(path_constants::get_project_root())
     }
-
-    pub fn get_lerobot_vulcan_dir() -> Result<PathBuf, String> {
-        let lerobot_vulcan_root = path_constants::get_lerobot_vulcan_root();
-        Ok(lerobot_vulcan_root)
-    }
-
-    // Helper function to convert PathBuf to Linux-compatible string
-    pub fn to_linux_path_string(path: &PathBuf) -> String {
-        path.to_string_lossy().replace('\\', "/")
-    }
 }
