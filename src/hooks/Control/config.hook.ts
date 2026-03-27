9016b65dec4d35f7e4125825a05f3e0fee9af26b
Nicholas Maselli <maselli.nicholas@gmail.com>
2026-03-27 19:03:28 -0400

    configs build clear

--- src/hooks/Control/config.hook.ts
+++ src/hooks/Control/config.hook.ts
@@ -1,5 +1,3 @@
-import type { Config } from '@/components/PageComponents/Robots/Config/RobotConfig';
-import { queryClient } from '@/hooks/default';
 import { getCalibrationModifiedAt, readCalibration } from '@/utils/config/calibration';
 import { useQuery } from '@tanstack/react-query';
 
@@ -14,36 +12,6 @@ export const CONTROL_CALIBRATION_MODIFIED_AT_KEY = (robot_type: string, nickname
     robot_type,
     nickname,
 ];
-
-//---------------------------------------------------------------------------------------------------//
-// Config Config
-//---------------------------------------------------------------------------------------------------//
-export const defaultConfig: Config = {
-    leader_arms: {
-        main: {
-            port: 'COM18',
-        },
-    },
-    follower_arms: {
-        main: {
-            port: 'COM21',
-        },
-    },
-    cameras: {
-        main: {
-            type: 'opencv',
-            camera_index: 0,
-            fps: 30,
-            width: 640,
-            height: 480,
-            color_mode: 'rgb',
-        },
-    },
-};
-
-export const getConfig = (nickname: string) => queryClient.getQueryData(CONTROL_CONFIG_KEY(nickname)) ?? defaultConfig;
-export const setConfig = (nickname: string, config: Config | null) => queryClient.setQueryData(CONTROL_CONFIG_KEY(nickname), config);
-export const useGetConfig = (nickname: string) => useQuery({ queryKey: CONTROL_CONFIG_KEY(nickname), queryFn: () => getConfig(nickname) });
 
 //---------------------------------------------------------------------------------------------------//
 // Calibration Config
