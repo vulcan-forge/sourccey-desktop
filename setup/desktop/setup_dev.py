b9cb66f148d1db8cdba21938fe093ab1b5946ff4
Nicholas Maselli <maselli.nicholas@gmail.com>
2026-07-10 13:42:34 -0400

    setup dev fix

--- setup/desktop/setup_dev.py
+++ setup/desktop/setup_dev.py
@@ -24,6 +24,10 @@ project_root = Path(__file__).resolve().parents[2]
 if str(project_root) not in sys.path:
     sys.path.insert(0, str(project_root))
 
+shared_dir = project_root / "setup" / "shared"
+if str(shared_dir) not in sys.path:
+    sys.path.insert(0, str(shared_dir))
+
 from setup.shared.setup_git import GitSetupManager  # type: ignore
 from setup.shared.setup_javascript import JavaScriptSetupManager  # type: ignore
 from setup.shared.setup_python import PythonSetupManager  # type: ignore
