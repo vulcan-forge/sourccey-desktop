d220d35944f5ea1cb53862b84b26dfc2a36cd1b8
Nicholas Maselli <maselli.nicholas@gmail.com>
2026-09-13 22:59:47 -0400

    battery update

--- src-tauri/src/modules/status/services/battery/battery_service.rs
+++ src-tauri/src/modules/status/services/battery/battery_service.rs
@@ -46,10 +46,8 @@ impl BatteryService {
 
         let lerobot_dir = DirectoryService::get_lerobot_vulcan_dir()?;
         let python_path = DirectoryService::get_python_path()?;
-        let script_path = lerobot_dir.join("src/lerobot/scripts/sourccey/battery/battery.py");
-
         let mut cmd = Command::new(python_path);
-        cmd.arg(&script_path);
+        cmd.arg("-m").arg("lerobot_robot_sourccey.battery.battery");
         configure_std_command(&mut cmd);
 
         let mut child = cmd
